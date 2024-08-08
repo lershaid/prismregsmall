760114ea37ca5240591cb98617d3c1686b7f2dc7
mac <mac@Lershaids-Macbook-Pro.local>
2024-08-08 15:58:50 +0300

    trying to add another thing to see if it would work or not with the map

--- layers/plot197d_4.js
+++ layers/plot197d_4.js
@@ -1,8 +1,30 @@
 var json_plot197d_4 = {
-"type": "FeatureCollection",
-"name": "plot197d_4",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ 58.907850034793697, 56.64009005938496 ], [ 58.92056977269165, 56.638739978331145 ], [ 58.91557161001996, 56.622862196766732 ], [ 58.899977206442756, 56.624385967375446 ], [ 58.907850034793697, 56.64009005938496 ] ] ] ] } }
-]
+    "type": "FeatureCollection",
+    "name": "plot197d_4",
+    "crs": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "id": null
+        },
+        "geometry": {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [58.907850034793697, 56.64009005938496],
+                        [58.92056977269165, 56.638739978331145],
+                        [58.91557161001996, 56.622862196766732],
+                        [58.899977206442756, 56.624385967375446],
+                        [58.907850034793697, 56.64009005938496]
+                    ]
+                ]
+            ]
+        }
+    }]
 }
